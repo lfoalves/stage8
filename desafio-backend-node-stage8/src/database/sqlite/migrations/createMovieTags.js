684230cdb11c9a7bbfcdc516932b8353bf325ce0
Luiz Fernando de Oliveira Alves <lfoalves.dev@gmail.com>
2022-10-17 19:50:44 -0400

    ajuste migration de movie_tags

--- desafio-backend-node-stage8/src/database/sqlite/migrations/createMovieTags.js
+++ desafio-backend-node-stage8/src/database/sqlite/migrations/createMovieTags.js
@@ -1,6 +1,6 @@
 const createMovieTags = `
   CREATE TABLE IF NOT EXISTS movie_tags (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     note_id VARCHAR REFERENCES movie_notes(id) ON DELETE CASCADE,
     user_id VARCHAR REFERENCES users(id),
     name VARCHAR NOT NULL
